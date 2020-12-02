e74f33d43a85983ec7e584811291baaf87020bad
unknown <sarkarmoumita14@gmail.com>
2020-12-02 20:51:08 +0530

    angry birds

---
+++ chain.js
@@ -0,0 +1,22 @@
+class Chain{
+    constructor(bodya,bodyb){
+        var options={
+            bodyA:bodya,
+            bodyB:bodyb,
+            stiffness:0.04,
+            length:10
+    }
+    this.chain=Matter.Constraint.create(options)
+    World.add(world,this.chain)
+    
+
+    }
+    display(){
+        var posA=this.chain.bodyA.position
+        var posB=this.chain.bodyB.position
+        push();
+        strokeWeight(5)
+line(posA.x,posA.y,posB.x,posB.y)
+        pop();
+    }
+}
